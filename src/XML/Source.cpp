68ca13aef83a91b39df1b59b8b1f60bb1920c108
Pol Jaumira <pjaumira@gmail.com>
2019-09-19 17:57:14 +0200

    Changes in class

--- src/XML/Source.cpp
+++ src/XML/Source.cpp
@@ -1,6 +1,13 @@
 //includes generics
-#include "iostream"
+#include <cstdlib>
+#include <iostream>
 #include <list>
+#include <string>
+#include <istream>
+#include <sstream>
+
+//Includes Docs
+#include "Enemies.cpp"
 
 //includes xml
 #include "../../dep/inc/XML/rapidxml.hpp"
@@ -10,15 +17,24 @@
 
 int main() {
 
-	//Codi doc practica
-
+	//Creacion Doc xml
 	rapidxml::xml_document<> doc;
-	std::ifstream file(".. / .. /res/files/config.xml");
+	std::ifstream file;
+	file.open("../../res/files/config.xml");
 	std::stringstream buffer;
 	buffer << file.rdbuf();
 	file.close();
 	std::string content(buffer.str());
 	doc.parse<0>(&content[0]);
+	//Puntero a la raiz
+	rapidxml::xml_node<> *pRoot = doc.first_node();
 
-	std::list <char> EnemyList;
-}
+	for (rapidxml::xml_node<> *pNode = pRoot->first_node("skeleton"); pNode; pNode->next_sibling()) {
+		std::cout << pNode->name() << ':' << '\n';
+		//imprimir els atributs del enemic
+		for (rapidxml::xml_node<> *pNode2 = pNode->first_node(); pNode2; pNode2 = pNode2->next_sibling()) {
+			std::cout << pNode2->name() << ':' << pNode2->value() << '\n';
+		};
+		std::cout << '\n';
+	}
+};
