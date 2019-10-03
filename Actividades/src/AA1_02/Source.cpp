bd54129c35ad09a41c426de03c220b6a3ec790dd
Pol Jaumira <pjaumira@gmail.com>
2019-10-03 17:44:52 +0200

    Classe3 SDL

--- Actividades/src/AA1_02/Source.cpp
+++ Actividades/src/AA1_02/Source.cpp
@@ -7,26 +7,22 @@
 #include <iostream>
 #include <string>
 
-//Game general information
+#include "Types.h"
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+bool hover = false;
 
 int main(int, char*[])
 {
 	// --- INIT SDL ---
-
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 		throw "No es pot inicialitzar SDL subsystems";
 
 	// --- WINDOW ---
-
 	SDL_Window *m_window{ SDL_CreateWindow("SDL...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
 	if (m_window == nullptr)
 		throw "No es pot inicialitzar SDL_Window";
 
 	// --- RENDERER ---
-
 	SDL_Renderer *m_renderer{ SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC) };
 	if (m_renderer == nullptr)
 		throw "No es pot inicialitzar SDL_Renderer";
@@ -36,34 +32,67 @@ int main(int, char*[])
 	if (!(IMG_Init(imgFlags) & imgFlags)) throw "Error: SDL_image init";
 
 	//-->SDL_TTF
-	if (!TTF_Init()) throw "No es pot inicialitzar SDL_ttf";
+	if(TTF_Init()==-1) throw "No es pot inicialitzar SDL_ttf";
 
 	//-->SDL_Mix
-
+	const Uint8 mixFlags{ MIX_INIT_MP3 };
+	if (!(Mix_Init(mixFlags)& mixFlags)) throw "Error amb SDL_Mixer init";
 
 	// --- SPRITES ---
+
 
 		//Background
 	SDL_Texture* bgTexture{ IMG_LoadTexture(m_renderer, "../../res/img/bg.jpg") };
 	if (bgTexture == nullptr) throw "Error: bgTexture init";
 	SDL_Rect bgRect{ 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
 
+		//Mouse
+	SDL_Texture *playerTexture{ IMG_LoadTexture(m_renderer, "../../res/img/kintoun.png") };
+	if (playerTexture == nullptr) throw "No s'ha pogut crear la textura";
+	SDL_Rect playerRect{ 0, 0, 100, 70 };
+
+	Vec2 mouse;
+
+
 	//-->Animated Sprite ---
 
 
 	// --- TEXT ---
-	/* TTF_Font *font{ TTF_OpenFont("../../res/ttf/sayan.ttf",80) };
+	//Cargar font
+	TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf",80) };
 	if (font == nullptr) throw "No es pot inicialitzar the TTF_Font";
-	SDL_Surface *tmpSurf {TTF_RenderText_Blended(font, "My first rodeo",SDL_Color{255,150,0,255})};
+
+	// Crear Surface
+	SDL_Surface *tmpSurf {TTF_RenderText_Blended(font, "Play",SDL_Color{255,150,0,255})};
 	if (tmpSurf == nullptr) throw "Unable to create the SDL text surface";
-	SDL_Texture *textTexture{ SDL_CreateTextureFromSurface(m_renderer, tmpSurf) };
-	SDL_Rect textRect{ 100,50, tmpSurf->w, tmpSurf->h };
+
+	//Crear Text_1 Play
+	SDL_Texture *textTexture1 { SDL_CreateTextureFromSurface(m_renderer, tmpSurf) };
+	SDL_Rect textRect1{ 100,50, tmpSurf->w, tmpSurf->h };
+
+	//Crear TextHover Play
+	tmpSurf { TTF_RenderText_Blended(font, "Play",SDL_Color{0,0,0,255}) };
+	SDL_Texture *textTexture2{ SDL_CreateTextureFromSurface(m_renderer, tmpSurf) };
+
+	//Crear Text_2 Sound
+	
+
+	//Crear Text_3 Exit
+
+
+	// Netejar
 	SDL_FreeSurface(tmpSurf);
-	TTF_CloseFont(font); */
+	TTF_CloseFont(font);
 
+	// --- AUDIO --- 
 
-	// --- AUDIO ---
-
+	if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024) == -1) {
+		throw "Unable to initialize SDL Mixer audio system";
+	}
+	Mix_Music *soundtrack{ Mix_LoadMUS("../../res/au/mainTheme.mp3") };
+	if (!soundtrack) throw "Unable to load the soundtrack";
+	Mix_VolumeMusic(MIX_MAX_VOLUME / 2);
+	Mix_PlayMusic(soundtrack, -1);
 
 	// --- GAME LOOP ---
 
@@ -79,12 +108,17 @@ int main(int, char*[])
 			case SDL_KEYDOWN:
 				if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false;
 				break;
+			case SDL_MOUSEMOTION:
+				mouse.x = event.motion.x;
+				mouse.y = event.motion.y;
+				break;
 			default:;
 			}
 		}
 
 		// UPDATE
-
+		playerRect.x += (mouse.x - playerRect.x - playerRect.w/2) / 10;
+		playerRect.y += (mouse.y - playerRect.y - playerRect.h/2) / 10;
 
 		// DRAW
 
@@ -93,24 +127,30 @@ int main(int, char*[])
 		//Background
 
 		SDL_RenderCopy(m_renderer, bgTexture, nullptr, &bgRect);
-		SDL_RenderPresent(m_renderer);
 
 		//Fonts
-		// SDL_RenderCopy(m_renderer, textTexture, nullptr, &textRect);
+		SDL_RenderCopy(m_renderer, textTexture1, nullptr, &textRect1);
+		
+		//Mouse
+		SDL_RenderCopy(m_renderer, playerTexture, nullptr, &playerRect);
+
+		SDL_RenderPresent(m_renderer);
 	}
 
 	// --- DESTROY ---
 
 	SDL_DestroyTexture(bgTexture);
-	// SDL_DestroyTexture(textTexture);
+	SDL_DestroyTexture(textTexture1);
+	SDL_DestroyTexture(playerTexture);
 	IMG_Quit();
 	SDL_DestroyRenderer(m_renderer);
 	SDL_DestroyWindow(m_window);
 
 	// --- QUIT ---
-
-	SDL_Quit();
+	Mix_CloseAudio();
+	Mix_Quit();
 	TTF_Quit();
+	SDL_Quit();
 
 	return 0;
 }
---
+++ Actividades/src/AA1_02/Types.h
@@ -0,0 +1,8 @@
+#pragma once
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+struct Vec2 {
+	int x = 0;
+	int y = 0;
+};
